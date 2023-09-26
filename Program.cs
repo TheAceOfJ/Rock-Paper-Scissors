bdee04c9e3a20f5afc9fe50cb8eb8d43e225596f
TheAceOfJ <arvid.jannhov@hotmail.com>
2023-09-26 15:21:43 +0200

    added players and name function

--- Program.cs
+++ Program.cs
@@ -1,1 +1,25 @@
-﻿Console.WriteLine("meow");
+﻿using Rock_Paper_Scissors;
+
+Console.WriteLine("Player 1, state your name:");
+string name1 = Console.ReadLine();
+Player player1 = new Player(name1);
+
+Console.WriteLine("Player 2, state your name:");
+string name2 = Console.ReadLine();
+Player player2 = new Player(name2);
+
+
+Console.WriteLine("Welcome " + player1.Name);
+Console.WriteLine("Welcome " + player2.Name);
+
+var lives = 3;
+
+while (lives != 0)
+{
+    Console.WriteLine("Player 1");
+    var p1 = Console.ReadLine();
+    if (p1.Contains("rock"))
+    {
+        Console.WriteLine("beans");
+    }
+}
---
+++ Status.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rock_Paper_Scissors
+{
+    public enum Status
+    {
+        win,
+        lose,
+        draw,
+    }
+    public enum choice
+    {
+        rock,
+        paper,
+        scissor,
+    }
+}
